20deb7fc5c677636763e20255383a4423ef91890
Ivan Neverovsky <ivan@neverovsky.com.ua>
2024-05-05 20:13:35 +0300

    added simple client

--- simple-client.js
+++ simple-client.js
@@ -1,1 +1,39 @@
 const http = require("node:http");
+
+const agent = new http.Agent({ keepAlive: true });
+
+const request = http.request({
+  agent,
+  hostname: "localhost",
+  port: 8050,
+  method: "POST",
+  path: "/create-post",
+  headers: {
+    "Content-Type": "application/json",
+    name: "Ivan",
+  },
+});
+
+request.on("response", (response) => {
+  console.log("------ STATUS: --------");
+  console.log(response.statusCode);
+
+  console.log("------ HEADERS: --------");
+  console.log(response.headers);
+
+  console.log("------ BODY: --------");
+  response.on("data", (chunk) => {
+    console.log(chunk.toString());
+  });
+
+  response.on("end", () => {
+    console.log("No more data in response");
+  });
+});
+
+request.end(
+  JSON.stringify({
+    title: "Post title",
+    body: "This is some text of the post and more and more...",
+  }),
+);
--- simple-server.js
+++ simple-server.js
@@ -12,10 +12,27 @@ server.on("request", (request, response) => {
   console.log("------ HEADERS: --------");
   console.log(request.headers);
 
+  const name = request.headers.name;
+
   console.log("------ BODY: --------");
 
+  let data = "";
+
   request.on("data", (chunk) => {
-    console.log(chunk.toString());
+    data += chunk.toString();
+  });
+
+  request.on("end", () => {
+    data = JSON.parse(data);
+    console.log(data);
+    console.log(name);
+
+    response.writeHead(200, { "Content-Type": "application/json" });
+    response.end(
+      JSON.stringify({
+        message: `Post with title ${data.title} was created by ${name}`,
+      }),
+    );
   });
 });
 
